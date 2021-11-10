43c1429bfdc4e340b5f84ff032a0fa1a22e5aec1
knshika <poojakumari17032001@gmail.com>
2021-11-10 15:50:47 +0530

    🐛 fixed ui a bit and width - color of pen

--- whiteboard/src/App.jsx
+++ whiteboard/src/App.jsx
@@ -1,75 +1,97 @@
-import { useLayoutEffect, useState } from 'react'
-
-import './App.css'
+import { useLayoutEffect, useState } from "react";
+import "./App.css";
+import Options from "./components/Options";
 
 function App() {
+  const [cursorStyles, setCursorStyles] = useState({
+    top: 0,
+    left: 0,
+  });
   const [canvas, setCanvas] = useState(null);
   const [ctx, setCtx] = useState(null);
   const [lineWidth, setLineWidth] = useState(5);
-  const [drawing, setDrawing] = useState(false)
-  const [lastX, setLastX] = useState(0)
-  const [lastY, setLastY] = useState(0)
-  const [restoreArray, setRestoreArray] = useState([])
-  const [color, setColor] = useState("#000000")
+  const [drawing, setDrawing] = useState(false);
+  const [lastX, setLastX] = useState(0);
+  const [lastY, setLastY] = useState(0);
+  const [restoreArray, setRestoreArray] = useState([]);
+  const [color, setColor] = useState("#000000");
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     setCanvas(canvas);
     setCtx(ctx);
-    canvas.height=window.innerHeight;
-    canvas.width=window.innerWidth;
-    ctx.styleStroke="#000080";
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    ctx.styleStroke = "#000080";
     ctx.lineCap = "round";
-   ctx.lineJoin = "round";
-   ctx.lineWidth = 5;
-  }, [])
- 
-  
-  const handleMouseMove=(e)=>{
-    if(!drawing) return;
-    ctx.strokeStyle=color;
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 5;
+  }, []);
+
+  const handleMouseMove = (e) => {
+    if (!drawing) return;
+    ctx.strokeStyle = color;
     ctx.beginPath();
-    ctx.moveTo(lastX,lastY);
-    ctx.lineTo(e.clientX,e.clientY);
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.clientX, e.clientY);
     ctx.stroke();
     setLastX(e.clientX);
     setLastY(e.clientY);
-
-    
-  }
-  const handleMouseDown=(e)=>{
+  };
+  const handleMouseDown = (e) => {
     setDrawing(true);
     setLastX(e.clientX);
     setLastY(e.clientY);
-  }
+  };
 
-  const handleMouseUp=(e)=>{
-    const img = ctx.getImageData(0,0,canvas.width,canvas.height);
-    setRestoreArray([...restoreArray,img]);
+  const handleMouseUp = (e) => {
+    const img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    setRestoreArray([...restoreArray, img]);
     setDrawing(false);
-  }
+  };
 
   return (
-    <div className="App"
-    //doubt
-    onMouseMove={(e)=>{
-      const cursor=document.querySelector(".cursor");
-      cursor.style.top = e.clientY+"px";
-      cursor.style.left = e.clientX+"px";
-      cursor.style.height = lineWidth+"px";
-      cursor.style.width = lineWidth+"px";
-      cursor.style.border = "1px solid" + color;
-    }}
+    <div
+      className="App"
+      //doubt
+      onMouseMove={(e) => {
+        // const cursor=document.querySelector(".cursor");
+        // cursor.style.top = e.clientY+"px";
+        // cursor.style.left = e.clientX+"px";
+        // cursor.style.height = lineWidth+"px";
+        // cursor.style.width = lineWidth+"px";
+        // cursor.style.border = "1px solid" + color;
+        setCursorStyles({
+          top: e.clientY,
+          left: e.clientX,
+        });
+      }}
     >
-      <div className="cursor"></div>
-      <canvas id="canvas"
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
+      <div
+        className="cursor"
+        style={{
+          border: `1px solid ${color}`,
+          height: lineWidth,
+          width: lineWidth,
+          ...cursorStyles,
+        }}
+      ></div>
+      <canvas
+        id="canvas"
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
       ></canvas>
-    </div>  
+      <Options
+        lineWidth={lineWidth}
+        setLineWidth={setLineWidth}
+        color={color}
+        setColor={setColor}
+        ctx={ctx}
+      />
+    </div>
   );
 }
 
-export default App
+export default App;
---
+++ whiteboard/src/components/Options.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/options.css";
+
+const Options = ({ lineWidth, setLineWidth, color, setColor, ctx }) => {
+  return (
+    <div className="options">
+      <div>
+        <label htmlFor="lineWidth">Width :</label>
+        <input
+          type="range"
+          name="Width"
+          id="lineWidth"
+          value={lineWidth}
+          onChange={(e) => {
+            setLineWidth(e.target.value);
+            ctx.lineWidth = lineWidth;
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="color">Color :</label>
+        <input
+          type="color"
+          name="Color"
+          id="color"
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+            ctx.color = color;
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Options;
